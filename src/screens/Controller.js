1d598befa3b9c071855671ec4b2e680354f2d0bc
unrealHimanshu <iamhbkulkarni@gmail.com>
2021-08-24 09:42:58 +0530

    (WIP) Add movieName filter

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Home from '../screens/home/Home';
+import React from "react";
+import Home from "../screens/home/Home";
 // import Details from "../screens/details/Details";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import BookShow from '../screens/bookshow/BookShow';
-import Confirmation from '../screens/confirmation/Confirmation';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import BookShow from "../screens/bookshow/BookShow";
+import Confirmation from "../screens/confirmation/Confirmation";
 
 const Controller = () => {
-  const baseUrl = '/api/v1/';
+  const baseUrl = "/api/v1/";
 
   return (
     <Router>
-      <div className='main-container'>
+      <div className="main-container">
         <Route
           exact
-          path='/'
+          path="/"
           render={(props) => <Home {...props} baseUrl={baseUrl} />}
         />
         {/* <Route
@@ -23,12 +23,12 @@ const Controller = () => {
         /> */}
         <Route
           exact
-          path='/bookshow/:id'
+          path="/bookshow/:id"
           render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
         />
         <Route
           exact
-          path='/confirm/:id'
+          path="/confirm/:id"
           render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
         />
       </div>
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,10 +6,29 @@ import "./Home.css";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 function Home(props) {
+  const theme = createMuiTheme();
+  const styles = {
+    cardHeading: {
+      color: theme.palette.primary.light,
+    },
+    cardItem: {
+      margin: theme.spacing.unit,
+    },
+  };
+
   const [upcomingMovies, setUpcomingMovies] = useState([]);
   const [releasedMovies, setReleasedMovies] = useState([]);
+  const [filters, setFilters] = useState({
+    movieName: "",
+  });
 
   const handleLogin = () => {
     console.log("handle login");
@@ -19,6 +38,11 @@ function Home(props) {
   };
   const handleBookShowFromHeader = () => {
     console.log("handle bookshow click from header");
+  };
+  const handleFormInput = (e) => {
+    const values = { ...filters };
+    values[e.target.name] = e.target.value;
+    setFilters(values);
   };
 
   const getUpcomingMovies = () => {
@@ -67,7 +91,7 @@ function Home(props) {
   }, []);
 
   return (
-    <div>
+    <div className="home-page-container">
       <Header
         isLoggedIn={false}
         loginHandler={handleLogin}
@@ -104,7 +128,34 @@ function Home(props) {
             ))}
           </GridList>
         </div>
-        <div className="filters-container"></div>
+        <div className="filters-container">
+          <Card>
+            <CardContent style={{ ...styles.cardHeading, ...styles.cardItem }}>
+              <span>FIND MOVIES BY:</span>
+            </CardContent>
+
+            <CardContent style={{ ...styles.cardItem }}>
+              <FormControl component="div" required className="input-field">
+                <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                <Input
+                  id="movieName"
+                  name="movieName"
+                  value={filters.movieName}
+                  onChange={handleFormInput}
+                />
+              </FormControl>
+              <FormControl component="div" required className="input-field">
+                <InputLabel htmlFor="movieName">Movie Name</InputLabel>
+                <Input
+                  id="movieName"
+                  name="movieName"
+                  value={filters.movieName}
+                  onChange={handleFormInput}
+                />
+              </FormControl>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
